a61f2928b9172a64c76242c7365a1ea4ab8c0fe4
kerloper <karimijavad990@gmail.com>
2023-07-16 13:16:46 +0330

    update sql

--- data/schema.sql
+++ data/schema.sql
@@ -16,25 +16,20 @@ CREATE TABLE IF NOT EXISTS `content_item`
 
 
 CREATE TABLE `content_meta_value` (
-                                      `id` int(10) UNSIGNED NOT NULL,
-                                      `item_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
-                                      `meta_key` varchar(64) NOT NULL DEFAULT '''''',
+                                      `id` int UNSIGNED NOT NULL,
+                                      `item_id` int UNSIGNED NOT NULL DEFAULT '0',
+                                      `item_slug` varchar(255) DEFAULT NULL,
+                                      `meta_key` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '''''',
                                       `value_string` varchar(255) NOT NULL DEFAULT '''''',
-                                      `value_id` varchar(255) NOT NULL DEFAULT '',
-                                      `value_number` int(10) UNSIGNED NOT NULL DEFAULT 0,
-                                      `status` int(10) UNSIGNED NOT NULL DEFAULT 0,
+                                      `value_number` int UNSIGNED NOT NULL DEFAULT '0',
+                                      `value_id` varchar(255) NOT NULL DEFAULT '''''',
+                                      `value_slug` varchar(255) DEFAULT NULL,
+                                      `status` int UNSIGNED NOT NULL DEFAULT '0',
                                       `logo` varchar(255) NOT NULL DEFAULT 'no-image.png',
-                                      `time_create` int(10) UNSIGNED NOT NULL DEFAULT 0,
-                                      `time_update` int(11) NOT NULL DEFAULT 0,
-                                      `time_delete` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `content_meta_key` (`id`, `key`, `value`, `type`, `suffix`, `logo`, `status`) VALUES
-                                                                                              (1, 'like', 'Like', 'int', '', 'no-image.png', 1),
-                                                                                              (2, 'dislike', 'Dislike', 'int', '', 'no-image.png', 1),
-                                                                                              (3, 'comment_count', 'Number of comments', 'int', '', 'no-image.png', 1),
-                                                                                              (4, 'category', 'Category', 'string', '', 'no-image.png', 1),
-                                                                                              (5, 'tag', 'Tag', 'string', '', 'no-image.png', 1);
+                                      `time_create` int UNSIGNED NOT NULL DEFAULT '0',
+                                      `time_update` int NOT NULL DEFAULT '0',
+                                      `time_delete` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 ALTER TABLE `content_meta_key`
     ADD PRIMARY KEY (`id`);
