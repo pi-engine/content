cc2327f442940c3a838ee6a423d36f80f6369e37
kerloper <karimijavad990@gmail.com>
2023-10-24 17:06:09 +0330

    tour list

--- src/Handler/Public/Tourism/Tour/ListHandler.php
+++ src/Handler/Public/Tourism/Tour/ListHandler.php
@@ -42,10 +42,23 @@ class ListHandler implements RequestHandlerInterface
         $requestBody = $request->getParsedBody();
 
         // Set record params
-        $requestBody['type'] = 'tour';
         $requestBody['user_id'] = $account['id'] ?? 0;
-        $result = $this->itemService->getItemList($requestBody, $account);
-
+        $params = [
+            'type' => 'tour',
+            'user_id' => $account['id'] ?? 0,
+        ];
+        $result = $this->itemService->getItemList($params, $account);
+        if($requestBody['type'] =='special_tours'){
+            $result['data']["middle_mode_banner"] = [
+                "title" => "تورهای خاص",
+                "abstract" => "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، ",
+                "button_title" => "مطالعه بیشتر",
+                "button_link" => "/special-tours/",
+                "video" => "",
+                "banner" => "https://yadapi.kerloper.com/upload/images/church-gh.jpg",
+                "has_video" => false
+            ];
+        }
         return new JsonResponse($result);
     }
 }
