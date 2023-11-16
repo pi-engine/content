c230cba84000c76c006a9baa6f63d0298a45de0e
kerloper <karimijavad990@gmail.com>
2023-11-16 20:53:26 +0330

    update travelogue list

--- src/Handler/Public/Tourism/Travelogue/TravelogueListHandler.php
+++ src/Handler/Public/Tourism/Travelogue/TravelogueListHandler.php
@@ -56,6 +56,100 @@ class TravelogueListHandler implements RequestHandlerInterface
                 "banner" => "https://yadapi.kerloper.com/upload/images/church-gh.jpg",
                 "has_video" => false
             ];
+            $result['data']["sample"] = $result['data']['list'][0];
+            ///TODO: remove connect to database in per item
+            $result['data']["destination"] = [
+                [
+                    "id" => 1,
+                    "slug" => "meta-category-bali",
+                    "title" => "بالی",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-bali",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-bali'])['data']['list'],
+                ],
+                [
+                    "id" => 2,
+                    "slug" => "meta-category-brazil",
+                    "title" => "برزیل",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-brazil",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-brazil'])['data']['list'],
+                ],
+                [
+                    "id" => 3,
+                    "slug" => "meta-category-peru",
+                    "title" => "پرو",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-peru",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-peru'])['data']['list'],
+                ],
+                [
+                    "id" => 4,
+                    "slug" => "meta-category-thailand",
+                    "title" => "تایلند",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-thailand",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-thailand'])['data']['list'],
+                ],
+                [
+                    "id" => 5,
+                    "slug" => "meta-category-srilanka",
+                    "title" => "سریلانکا",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-srilanka",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-srilanka'])['data']['list'],
+                ],
+                [
+                    "id" => 6,
+                    "slug" => "meta-category-kenya",
+                    "title" => "کنیا",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-kenya",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-kenya'])['data']['list'],
+                ],
+                [
+                    "id" => 7,
+                    "slug" => "meta-category-cuba",
+                    "title" => "کوبا",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-cuba",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-cuba'])['data']['list'],
+                ],
+                [
+                    "id" => 8,
+                    "slug" => "meta-category-maldives",
+                    "title" => "مالدیو",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-maldives",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-maldives'])['data']['list'],
+                ],
+                [
+                    "id" => 9,
+                    "slug" => "meta-category-malaysia",
+                    "title" => "مالزی",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-malaysia",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-malaysia'])['data']['list'],
+                ],
+                [
+                    "id" => 10,
+                    "slug" => "meta-category-india",
+                    "title" => "هند",
+                    "type" => "category",
+                    "icon" => "",
+                    "value" => "meta-category-india",
+                    "list" => $this->itemService->getItemList(['type'=>'travelogue','categories' => 'meta-category-india'])['data']['list'],
+                ]
+            ];
         }
         return new JsonResponse($result);
     }
