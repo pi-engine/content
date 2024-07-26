32501137f60b94bbc881e51c629f706efab74dca
kerloper <karimijavad990@gmail.com>
2024-07-26 11:18:41 +0330

    add parent_id

--- src/Model/Item.php
+++ src/Model/Item.php
@@ -5,6 +5,7 @@ namespace Content\Model;
 class Item
 {
     private mixed  $id;
+    private mixed  $parent_id;
     private string $title;
     private string $slug;
     private string $type;
@@ -19,6 +20,7 @@ class Item
 
     /**
      * @param mixed $id
+     * @param mixed $parent_id
      * @param string $title
      * @param string $slug
      * @param string $type
@@ -30,9 +32,10 @@ class Item
      * @param string $information
      * @param mixed $priority
      */
-    public function __construct(mixed $id, string $title, string $slug, string $type, int $status, int $user_id, int $time_create, int $time_update, int $time_delete, string $information, mixed $priority)
+    public function __construct(mixed $id, mixed $parent_id, string $title, string $slug, string $type, int $status, int $user_id, int $time_create, int $time_update, int $time_delete, string $information, mixed $priority)
     {
         $this->id = $id;
+        $this->parent_id = $parent_id;
         $this->title = $title;
         $this->slug = $slug;
         $this->type = $type;
@@ -45,181 +48,124 @@ class Item
         $this->priority = $priority;
     }
 
-    /**
-     * @return mixed
-     */
     public function getId(): mixed
     {
         return $this->id;
     }
 
-    /**
-     * @param mixed $id
-     */
     public function setId(mixed $id): void
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
+    public function getParentId(): mixed
+    {
+        return $this->parent_id;
+    }
+
+    public function setParentId(mixed $parent_id): void
+    {
+        $this->parent_id = $parent_id;
+    }
+
     public function getTitle(): string
     {
         return $this->title;
     }
 
-    /**
-     * @param string $title
-     */
     public function setTitle(string $title): void
     {
         $this->title = $title;
     }
 
-    /**
-     * @return string
-     */
     public function getSlug(): string
     {
         return $this->slug;
     }
 
-    /**
-     * @param string $slug
-     */
     public function setSlug(string $slug): void
     {
         $this->slug = $slug;
     }
 
-    /**
-     * @return string
-     */
     public function getType(): string
     {
         return $this->type;
     }
 
-    /**
-     * @param string $type
-     */
     public function setType(string $type): void
     {
         $this->type = $type;
     }
 
-    /**
-     * @return int
-     */
     public function getStatus(): int
     {
         return $this->status;
     }
 
-    /**
-     * @param int $status
-     */
     public function setStatus(int $status): void
     {
         $this->status = $status;
     }
 
-    /**
-     * @return int
-     */
     public function getUserId(): int
     {
         return $this->user_id;
     }
 
-    /**
-     * @param int $user_id
-     */
     public function setUserId(int $user_id): void
     {
         $this->user_id = $user_id;
     }
 
-    /**
-     * @return int
-     */
     public function getTimeCreate(): int
     {
         return $this->time_create;
     }
 
-    /**
-     * @param int $time_create
-     */
     public function setTimeCreate(int $time_create): void
     {
         $this->time_create = $time_create;
     }
 
-    /**
-     * @return int
-     */
     public function getTimeUpdate(): int
     {
         return $this->time_update;
     }
 
-    /**
-     * @param int $time_update
-     */
     public function setTimeUpdate(int $time_update): void
     {
         $this->time_update = $time_update;
     }
 
-    /**
-     * @return int
-     */
     public function getTimeDelete(): int
     {
         return $this->time_delete;
     }
 
-    /**
-     * @param int $time_delete
-     */
     public function setTimeDelete(int $time_delete): void
     {
         $this->time_delete = $time_delete;
     }
 
-    /**
-     * @return string
-     */
     public function getInformation(): string
     {
         return $this->information;
     }
 
-    /**
-     * @param string $information
-     */
     public function setInformation(string $information): void
     {
         $this->information = $information;
     }
 
-    /**
-     * @return mixed
-     */
     public function getPriority(): mixed
     {
         return $this->priority;
     }
 
-    /**
-     * @param mixed $priority
-     */
     public function setPriority(mixed $priority): void
     {
         $this->priority = $priority;
     }
-
 
 }
