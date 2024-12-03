9887a57c617332d396ea907eb21e2a94b5309c51
kerloper <karimijavad990@gmail.com>
2024-12-03 13:41:37 +0330

    add address information handler

--- src/Handler/Public/Information/InformationAddressHandler.php
+++ src/Handler/Public/Information/InformationAddressHandler.php
@@ -36,201 +36,213 @@ class InformationAddressHandler implements RequestHandlerInterface
     /// TODO: check slug and check public access
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        // Get request body
+        $requestBody = $request->getParsedBody();
+
+
         $result = [
             [
-                "name" => "تهران",
-                "slug" => "tehran",
-                "level" => "state",
-                "level_name" => "استان",
+                "name" => "ایران",
+                "slug" => "iran",
+                "level" => "country",
+                "level_name" => "کشور",
                 "children" => [
+
                     [
                         "name" => "تهران",
-                        "slug" => "tehran-city",
-                        "level" => "city",
-                        "level_name" => "شهر",
+                        "slug" => "tehran",
+                        "level" => "state",
+                        "level_name" => "استان",
                         "children" => [
                             [
-                                "name" => "پاسداران",
-                                "slug" => "pasdaran",
-                                "level" => "district",
-                                "level_name" => "محله"
+                                "name" => "تهران",
+                                "slug" => "tehran-city",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                    [
+                                        "name" => "پاسداران",
+                                        "slug" => "pasdaran",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ],
+                                    [
+                                        "name" => "شهرک غرب",
+                                        "slug" => "shahrak-gharb",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ]
+                                ]
                             ],
                             [
-                                "name" => "شهرک غرب",
-                                "slug" => "shahrak-gharb",
-                                "level" => "district",
-                                "level_name" => "محله"
+                                "name" => "ری",
+                                "slug" => "rey",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                    [
+                                        "name" => "عبدالعظیم",
+                                        "slug" => "abdolazim",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ]
+                                ]
                             ]
                         ]
                     ],
-                    [
-                        "name" => "ری",
-                        "slug" => "rey",
-                        "level" => "city",
-                        "level_name" => "شهر",
-                        "children" => [
-                            [
-                                "name" => "عبدالعظیم",
-                                "slug" => "abdolazim",
-                                "level" => "district",
-                                "level_name" => "محله"
-                            ]
-                        ]
-                    ]
-                ]
-            ],
-            [
-                "name" => "اصفهان",
-                "slug" => "isfahan",
-                "level" => "state",
-                "level_name" => "استان",
-                "children" => [
                     [
                         "name" => "اصفهان",
-                        "slug" => "isfahan-city",
-                        "level" => "city",
-                        "level_name" => "شهر",
+                        "slug" => "isfahan",
+                        "level" => "state",
+                        "level_name" => "استان",
                         "children" => [
                             [
-                                "name" => "احمدآباد",
-                                "slug" => "ahmadabad",
-                                "level" => "district",
-                                "level_name" => "محله"
+                                "name" => "اصفهان",
+                                "slug" => "isfahan-city",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                    [
+                                        "name" => "احمدآباد",
+                                        "slug" => "ahmadabad",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ],
+                                    [
+                                        "name" => "چهارباغ",
+                                        "slug" => "chaharbagh",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ]
+                                ]
                             ],
                             [
-                                "name" => "چهارباغ",
-                                "slug" => "chaharbagh",
-                                "level" => "district",
-                                "level_name" => "محله"
+                                "name" => "کاشان",
+                                "slug" => "kashan",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                    [
+                                        "name" => "فین",
+                                        "slug" => "fin",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ]
+                                ]
                             ]
                         ]
                     ],
                     [
-                        "name" => "کاشان",
-                        "slug" => "kashan",
-                        "level" => "city",
-                        "level_name" => "شهر",
+                        "name" => "خراسان رضوی",
+                        "slug" => "khorasan-razavi",
+                        "level" => "state",
+                        "level_name" => "استان",
                         "children" => [
                             [
-                                "name" => "فین",
-                                "slug" => "fin",
-                                "level" => "district",
-                                "level_name" => "محله"
+                                "name" => "مشهد",
+                                "slug" => "mashhad",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                    [
+                                        "name" => "طرقبه",
+                                        "slug" => "torghabe",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ],
+                                    [
+                                        "name" => "شاندیز",
+                                        "slug" => "shandiz",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ]
+                                ]
+                            ],
+                            [
+                                "name" => "نیشابور",
+                                "slug" => "neyshabur",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                ]
+                            ]
+                        ]
+                    ],
+                    [
+                        "name" => "فارس",
+                        "slug" => "fars",
+                        "level" => "state",
+                        "level_name" => "استان",
+                        "children" => [
+                            [
+                                "name" => "شیراز",
+                                "slug" => "shiraz",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                    [
+                                        "name" => "قصرالدشت",
+                                        "slug" => "ghasr-dasht",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ],
+                                    [
+                                        "name" => "معالی آباد",
+                                        "slug" => "maali-abad",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ]
+                                ]
+                            ],
+                            [
+                                "name" => "مرودشت",
+                                "slug" => "marvdasht",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                ]
+                            ]
+                        ]
+                    ],
+                    [
+                        "name" => "خوزستان",
+                        "slug" => "khuzestan",
+                        "level" => "state",
+                        "level_name" => "استان",
+                        "children" => [
+                            [
+                                "name" => "اهواز",
+                                "slug" => "ahvaz",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                    [
+                                        "name" => "کوی ملت",
+                                        "slug" => "kuy-mellat",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ],
+                                    [
+                                        "name" => "کوی گلستان",
+                                        "slug" => "kuy-golestan",
+                                        "level" => "district",
+                                        "level_name" => "محله"
+                                    ]
+                                ]
+                            ],
+                            [
+                                "name" => "آبادان",
+                                "slug" => "abadan",
+                                "level" => "city",
+                                "level_name" => "شهر",
+                                "children" => [
+                                ]
                             ]
                         ]
                     ]
                 ]
             ],
-            [
-                "name" => "خراسان رضوی",
-                "slug" => "khorasan-razavi",
-                "level" => "state",
-                "level_name" => "استان",
-                "children" => [
-                    [
-                        "name" => "مشهد",
-                        "slug" => "mashhad",
-                        "level" => "city",
-                        "level_name" => "شهر",
-                        "children" => [
-                            [
-                                "name" => "طرقبه",
-                                "slug" => "torghabe",
-                                "level" => "district",
-                                "level_name" => "محله"
-                            ],
-                            [
-                                "name" => "شاندیز",
-                                "slug" => "shandiz",
-                                "level" => "district",
-                                "level_name" => "محله"
-                            ]
-                        ]
-                    ],
-                    [
-                        "name" => "نیشابور",
-                        "slug" => "neyshabur",
-                        "level" => "city",
-                        "level_name" => "شهر",
-                        "children" => [
-                        ]
-                    ]
-                ]
-            ],
-            [
-                "name" => "فارس",
-                "slug" => "fars",
-                "level" => "state",
-                "level_name" => "استان",
-                "children" => [
-                    [
-                        "name" => "شیراز",
-                        "slug" => "shiraz",
-                        "level" => "city",
-                        "level_name" => "شهر",
-                        "children" => [
-                            [
-                                "name" => "قصرالدشت",
-                                "slug" => "ghasr-dasht",
-                                "level" => "district",
-                                "level_name" => "محله"
-                            ],
-                            [
-                                "name" => "معالی آباد",
-                                "slug" => "maali-abad",
-                                "level" => "district",
-                                "level_name" => "محله"
-                            ]
-                        ]
-                    ],
-                    [
-                        "name" => "مرودشت",
-                        "slug" => "marvdasht",
-                        "level" => "city",
-                        "level_name" => "شهر",
-                        "children" => [
-                        ]
-                    ]
-                ]
-            ],
-            [
-                "name" => "خوزستان",
-                "slug" => "khuzestan",
-                "level" => "state",
-                "level_name" => "استان",
-                "children" => [
-                    [
-                        "name" => "اهواز",
-                        "slug" => "ahvaz",
-                        "level" => "city",
-                        "level_name" => "شهر",
-                        "children" => [
-                            [
-                                "name" => "کوی ملت",
-                                "slug" => "kuy-mellat",
-                                "level" => "district",
-                                "level_name" => "محله"
-                            ],
-                            [
-                                "name" => "کوی گلستان",
-                                "slug" => "kuy-golestan",
-                                "level" => "district",
-                                "level_name" => "محله"
-                            ]
-                        ]
-                    ],
-                    [
-                        "name" => "آبادان",
-                        "slug" => "abadan",
-                        "level" => "city",
-                        "level_name" => "شهر",
-                        "children" => [
-                        ]
-                    ]
-                ]
-            ]
         ];
-
 
 
         // Set result
